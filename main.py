e14ff2353f7bb702277c21bc59948b71b9cd6a15
Bruno <141152582+BrUn0-P1nh41r0@users.noreply.github.com>
2025-01-11 15:04:40 +0000

    get, replace and create files

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-#with open("my_file.txt") as file:
-#    content = file.read()
-#    print(content)
+PLACEHOLDER = "[name]"
 
-with open("new_file.txt", mode="a") as file:
-    file.write("\nNew Text.")
+with open("input/Names/invited_names.txt") as file:
+    content = file.readlines()
+with open("input/Letters/starting_letter.txt") as letter:
+    invite = letter.read()
+
+for line in content:
+    name = line.strip()
+    personalized_invite = invite.replace(PLACEHOLDER, name)
+    with open(f"output/readyToSend/letter_for_{name}.txt", "w") as final_letter:
+        final_letter.write(personalized_invite)
+
